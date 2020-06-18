6787089fda47898a1b897bacb5a9c72e5a1cf586
basamaryan <66957409+basamaryan@users.noreply.github.com>
2020-06-18 22:20:27 +0530

    Add files via upload

---
+++ DivNum/DivNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int secNum = Convert.ToInt32(Console.ReadLine());
+            double ANS = (double)firstNum / secNum;
+            Console.WriteLine("The answer is " + ANS);
+            Console.Read();
+        }
+          
+        
+    }
+}
