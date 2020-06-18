83f61b60ff025b2676bfcd25c7587497555dfeda
basamaryan <66957409+basamaryan@users.noreply.github.com>
2020-06-18 22:31:11 +0530

    Add files via upload

---
+++ 1.6/1.6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int secNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Third number:");
+            int thirdNum = Convert.ToInt32(Console.ReadLine());
+            int ANS = firstNum * secNum * thirdNum;
+            Console.WriteLine("The answer is " + ANS);
+            Console.Read();
+        }
+        
+        }
+    }
