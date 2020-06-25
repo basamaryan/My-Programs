b57add083c7582e0235ceccada7603076b1dfd40
basamaryan <66957409+basamaryan@users.noreply.github.com>
2020-06-25 21:18:52 +0530

    Add files via upload

---
+++ 3.4/3.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number");
+            string temp = (Console.ReadLine());
+            int numEntered = Convert.ToInt32(temp);
+            for (int i = 0; i < 11; i++) 
+            {
+                Console.WriteLine(numEntered + "*" + i + "=" + (numEntered * i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
