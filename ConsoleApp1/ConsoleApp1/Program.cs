af7df6b1509ecaf5b276fdd6dce178d7375cdd9f
basamaryan <66957409+basamaryan@users.noreply.github.com>
2020-06-25 21:20:15 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Enter a new word");
+            string newWord = (Console.ReadLine());
+            int length = (newWord.Length);
+            string temp = ("");
+            temp = newWord(0);
+            newWord(0) = newWord(length -1);
+            newWord(length - 1) = temp; */
+            // why does this give an error sir?
+
+
+
+
+
+
+        }
+    }
+}
