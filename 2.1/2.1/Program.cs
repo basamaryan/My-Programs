02a81fae2fb3beb6c67d0e050cec898a30f7d396
basamaryan <66957409+basamaryan@users.noreply.github.com>
2020-06-25 19:03:06 +0530

    Add files via upload

---
+++ 2.1/2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the word");
+            string wordEntered = (Console.ReadLine());
+            Console.Write("Enter the number");
+            int numVal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(wordEntered.Remove(numVal, 1));
+            Console.Read();
+
+
+
+
+        }
+    }
+}
