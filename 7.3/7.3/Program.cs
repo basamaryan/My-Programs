96e7af414691e6341e421ce39d228c73736adfeb
Aryan <66957409+basamaryan@users.noreply.github.com>
2020-07-12 03:08:32 +0530

    Add files via upload

---
+++ 7.3/7.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = new List<int>();
+            Console.WriteLine("Enter a quantity: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= quantity; i++)
+            {
+                Console.WriteLine("Enter a number: ");
+                int temp = Convert.ToInt32(Console.ReadLine());
+                numList.Add(temp);
+            }
+            int maxInd = numList.IndexOf(numList.Max());
+            Console.WriteLine("The index of maximum value is: " + maxInd);
+
+            Console.ReadLine();
+        }
+    }
+}
